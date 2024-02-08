ef3defc63457b8c4717e7c4cd35e3bb47b411d89
pedro <pedrogermano232@gmail.com>
2024-02-08 14:53:25 -0300

    card

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import logo from "./assets/logo-nlw-expert.svg";
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12">
+    <div className="max-w-6xl mx-auto my-12 space-y-6">
       <img src={logo} alt="logo" />
-      <input type="text" placeholder="Busque as suas notas..." />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque as suas notas..."
+          className="w-full text-3xl font-semibold tracking-tight bg-transparent outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700"></div>
+        <div className="rounded-md bg-slate-800"></div>
+        <div className="rounded-md bg-slate-800"></div>
+      </div>
     </div>
-  )
+  );
 }
