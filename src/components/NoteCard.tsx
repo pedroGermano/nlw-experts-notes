b7fe2a3d88f29948a9e25bf0b390e6dbea455175
pedro <pedrogermano232@gmail.com>
2024-02-08 19:41:15 -0300

    Dialog

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,14 +1,23 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="relative p-5 space-y-3 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 hover:cursor-pointer focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
-        totam nemo. Distinctio optio unde fuga, delectus eius rerum ratione.
-        Saepe pariatur reiciendis numquam, itaque repudiandae quis sed. Itaque,
-        ex atque?
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 point-events-none" />
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="relative flex flex-col gap-3 p-5 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 hover:cursor-pointer focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 point-events-none" />
+      </Dialog.Trigger>
+      <Dialog.Content>Oi</Dialog.Content>
+    </Dialog.Root>
   );
 }
