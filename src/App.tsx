7eb09f938373c3dc5231c4ce9185e3a5e0b0915e
pedro <pedrogermano232@gmail.com>
2024-02-10 23:24:54 -0300

    card new dinamic add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/NoteCard";
 
 export function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      date: new Date(),
+      content: "Hello World",
+    },
+    {
+      id: 2,
+      date: new Date(),
+      content: "Nota",
+    }
+  ])
   return (
     <div className="max-w-6xl mx-auto my-12 space-y-6">
       <img src={logo} alt="logo" />
@@ -20,12 +33,9 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: "Hello World",
-          }}
-        />
+        {notes.map(note =>  {
+          return <NoteCard note={note}/>
+        })}
       </div>
     </div>
   );
